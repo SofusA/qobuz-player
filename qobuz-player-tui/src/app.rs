SofusA/qobuz-player

commit 8173930c615135cf839048a03b543df4d7b9baa3

List widgets

qobuz-player-tui/src/widgets/playlist_list.rs
@@ -1,0 +1,144 @@
+use qobuz_player_controls::{client::Client, notification::Notification};
+use qobuz_player_models::Playlist;
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::KeyCode,
+    layout::Rect,
+    widgets::{Row, StatefulWidget},
+};
+
+use crate::{
+    app::{FilteredListState, NotificationList, Output},
+    popup::{DeletePlaylistPopupstate, NewPlaylistPopupState, PlaylistPopupState, Popup},
+    ui::{basic_list_table, mark_as_owned},
+};
+
+#[derive(Default)]
+pub struct PlaylistList {
+    items: FilteredListState<Playlist>,
+}
+
+impl PlaylistList {
+    pub fn new(playlists: Vec<Playlist>) -> Self {
+        let playlists = FilteredListState::new(playlists);
+        Self { items: playlists }
+    }
+
+    pub fn render(&mut self, area: Rect, buf: &mut Buffer) {
+        let table = basic_list_table(
+            self.items
+                .filter()
+                .iter()
+                .map(|playlist| {
+                    Row::new(vec![mark_as_owned(
+                        playlist.title.clone(),
+                        playlist.is_owned,
+                    )])
+                })
+                .collect::<Vec<_>>(),
+        );
+        table.render(area, buf, &mut self.items.state);
+    }
+
+    pub fn set_filter(&mut self, items: Vec<Playlist>) {
+        self.items.set_filter(items);
+    }
+
+    pub fn all_items(&self) -> &Vec<Playlist> {
+        self.items.all_items()
+    }
+
+    pub fn set_all_items(&mut self, items: Vec<Playlist>) {
+        self.items.set_all_items(items);
+    }
+
+    pub async fn handle_events(
+        &mut self,
+        event: KeyCode,
+        client: &Client,
+        notifications: &mut NotificationList,
+    ) -> Output {
+        match event {
+            KeyCode::Down | KeyCode::Char('j') => {
+                self.items.state.select_next();
+                Output::Consumed
+            }
+
+            KeyCode::Up | KeyCode::Char('k') => {
+                self.items.state.select_previous();
+                Output::Consumed
+            }
+
+            KeyCode::Char('C') => Output::Popup(Popup::NewPlaylist(NewPlaylistPopupState::new())),
+
+            KeyCode::Char('A') => {
+                let index = self.items.state.selected();
+                let selected = index.and_then(|index| self.items.filter().get(index));
+
+                if let Some(selected) = selected
+                    && !selected.is_owned
+                {
+                    return match client.add_favorite_playlist(selected.id).await {
+                        Ok(_) => {
+                            notifications.push(Notification::Info(format!(
+                                "{} added to favorites",
+                                selected.title
+                            )));
+                            Output::UpdateFavorites
+                        }
+                        Err(err) => Output::Error(err.to_string()),
+                    };
+                }
+
+                Output::Consumed
+            }
+
+            KeyCode::Char('D') => {
+                let index = self.items.state.selected();
+                let selected = index.and_then(|index| self.items.filter().get(index));
+
+                if let Some(selected) = selected {
+                    match selected.is_owned {
+                        true => {
+                            return Output::Popup(Popup::DeletePlaylist(
+                                DeletePlaylistPopupstate::new(selected.clone()),
+                            ));
+                        }
+                        false => {
+                            return match client.remove_favorite_playlist(selected.id).await {
+                                Ok(_) => {
+                                    notifications.push(Notification::Info(format!(
+                                        "{} removed from favorites",
+                                        selected.title
+                                    )));
+                                    Output::UpdateFavorites
+                                }
+                                Err(err) => Output::Error(err.to_string()),
+                            };
+                        }
+                    }
+                }
+
+                Output::Consumed
+            }
+
+            KeyCode::Enter => {
+                let index = self.items.state.selected();
+                let selected = index.and_then(|index| self.items.filter().get(index));
+
+                let Some(selected) = selected else {
+                    return Output::Consumed;
+                };
+
+                let playlist = match client.playlist(selected.id).await {
+                    Ok(res) => res,
+                    Err(err) => return Output::Error(err.to_string()),
+                };
+
+                Output::Popup(Popup::Playlist(PlaylistPopupState::new(playlist)))
+            }
+
+            _ => Output::NotConsumed,
+        }
+    }
+}

qobuz-player-tui/src/widgets/track_list.rs
@@ -1,0 +1,197 @@
+use qobuz_player_controls::{client::Client, controls::Controls, notification::Notification};
+use qobuz_player_models::Track;
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::KeyCode,
+    layout::{Constraint, Rect},
+    style::{Modifier, Stylize},
+    text::Line,
+    widgets::{Row, StatefulWidget, Table},
+};
+
+use crate::{
+    app::{FilteredListState, NotificationList, Output},
+    ui::{ROW_HIGHLIGHT_STYLE, mark_explicit_and_hifi},
+};
+
+#[derive(Default)]
+pub struct TrackList {
+    items: FilteredListState<Track>,
+}
+
+pub enum TrackListEvent {
+    Track,
+    Album(String),
+    Playlist(u32, bool),
+    Artist(u32),
+}
+
+impl TrackList {
+    pub fn new(tracks: Vec<Track>) -> Self {
+        let tracks = FilteredListState::new(tracks);
+        Self { items: tracks }
+    }
+
+    pub fn render(&mut self, area: Rect, buf: &mut Buffer) {
+        let table = track_table(self.items.filter());
+        table.render(area, buf, &mut self.items.state);
+    }
+
+    pub fn select_first(&mut self) {
+        self.items.state.select(Some(0));
+    }
+
+    pub fn set_all_items(&mut self, items: Vec<Track>) {
+        self.items.set_all_items(items);
+    }
+
+    pub fn filter(&self) -> &Vec<Track> {
+        self.items.filter()
+    }
+
+    pub async fn handle_events(
+        &mut self,
+        event: KeyCode,
+        client: &Client,
+        controls: &Controls,
+        notifications: &mut NotificationList,
+        event_type: TrackListEvent,
+    ) -> Output {
+        match event {
+            KeyCode::Down | KeyCode::Char('j') => {
+                self.items.state.select_next();
+                Output::Consumed
+            }
+
+            KeyCode::Up | KeyCode::Char('k') => {
+                self.items.state.select_previous();
+                Output::Consumed
+            }
+
+            KeyCode::Char('a') => {
+                let index = self.items.state.selected();
+
+                let track = index.and_then(|index| self.items.filter().get(index));
+
+                if let Some(id) = track {
+                    return Output::AddTrackToPlaylist(id.clone());
+                }
+                Output::Consumed
+            }
+
+            KeyCode::Char('N') => {
+                let index = self.items.state.selected();
+                let selected = index.and_then(|index| self.items.filter().get(index));
+
+                let Some(selected) = selected else {
+                    return Output::Consumed;
+                };
+
+                controls.play_track_next(selected.id);
+                Output::Consumed
+            }
+
+            KeyCode::Char('B') => {
+                let index = self.items.state.selected();
+                let selected = index.and_then(|index| self.items.filter().get(index));
+
+                if let Some(selected) = selected {
+                    controls.add_track_to_queue(selected.id);
+                };
+
+                Output::Consumed
+            }
+
+            KeyCode::Char('A') => {
+                let index = self.items.state.selected();
+                let selected = index.and_then(|index| self.items.filter().get(index));
+
+                if let Some(selected) = selected {
+                    return match client.add_favorite_track(selected.id).await {
+                        Ok(_) => {
+                            notifications.push(Notification::Info(format!(
+                                "{} added to favorites",
+                                selected.title
+                            )));
+                            Output::UpdateFavorites
+                        }
+                        Err(err) => Output::Error(err.to_string()),
+                    };
+                }
+                Output::Consumed
+            }
+
+            KeyCode::Char('D') => {
+                let index = self.items.state.selected();
+                let selected = index.and_then(|index| self.items.filter().get(index));
+
+                if let Some(selected) = selected {
+                    return match client.remove_favorite_track(selected.id).await {
+                        Ok(_) => {
+                            notifications.push(Notification::Info(format!(
+                                "{} removed from favorites",
+                                selected.title
+                            )));
+                            Output::UpdateFavorites
+                        }
+                        Err(err) => Output::Error(err.to_string()),
+                    };
+                }
+                Output::Consumed
+            }
+
+            KeyCode::Enter => {
+                let Some(index) = self.items.state.selected() else {
+                    return Output::Consumed;
+                };
+
+                match event_type {
+                    TrackListEvent::Track => {
+                        let selected = self.items.filter().get(index);
+                        if let Some(selected) = selected {
+                            controls.play_track(selected.id);
+                        }
+                    }
+                    TrackListEvent::Album(id) => controls.play_album(&id, index),
+                    TrackListEvent::Playlist(id, shuffle) => {
+                        controls.play_playlist(id, index, shuffle)
+                    }
+                    TrackListEvent::Artist(id) => controls.play_top_tracks(id, index),
+                }
+
+                Output::Consumed
+            }
+
+            _ => Output::NotConsumed,
+        }
+    }
+}
+
+fn track_table<'a>(rows: &[Track]) -> Table<'a> {
+    let rows: Vec<_> = rows
+        .iter()
+        .map(|track| {
+            Row::new(vec![
+                mark_explicit_and_hifi(track.title.clone(), track.explicit, track.hires_available),
+                Line::from(track.artist_name.clone().unwrap_or_default()),
+                Line::from(track.album_title.clone().unwrap_or_default()),
+            ])
+        })
+        .collect();
+
+    let is_empty = rows.is_empty();
+    let mut table = Table::new(
+        rows,
+        [
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+        ],
+    )
+    .row_highlight_style(ROW_HIGHLIGHT_STYLE);
+
+    if !is_empty {
+        table = table.header(Row::new(["Title", "Artist", "Album"]).add_modifier(Modifier::BOLD));
+    }
+    table
+}

qobuz-player-controls/src/client.rs
@@ -113,7 +113,7 @@ impl Client {
         })
     }
 
-    pub(crate) async fn track_url(&self, track_id: u32) -> Result<TrackURL> {
+    pub async fn track_url(&self, track_id: u32) -> Result<TrackURL> {
         let client = self.get_client().await?;
         Ok(client.track_url(track_id).await?)
     }

qobuz-player-controls/src/lib.rs
@@ -8,12 +8,12 @@ pub use qobuz_player_client::client::AudioQuality;
 pub mod client;
 pub mod controls;
 pub mod database;
-pub(crate) mod downloader;
+pub mod downloader;
 pub mod error;
 pub mod notification;
 pub mod player;
-pub(crate) mod simple_cache;
-pub(crate) mod sink;
+pub mod simple_cache;
+pub mod sink;
 pub mod tracklist;
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;

qobuz-player-controls/src/player.rs
@@ -14,7 +14,7 @@ use crate::{
     controls::{ControlCommand, Controls},
     database::Database,
     downloader::Downloader,
-    notification::NotificationBroadcast,
+    notification::{Notification, NotificationBroadcast},
     sink::QueryTrackResult,
     tracklist::{SingleTracklist, TracklistType},
 };
@@ -406,24 +406,35 @@ impl Player {
         let mut tracklist = self.tracklist_rx.borrow().clone();
 
         tracklist.queue.remove(index);
-        self.update_queue(tracklist).await
+        self.update_queue(tracklist).await?;
+        let notification = Notification::Info("Queue updated".into());
+        self.broadcast.send(notification);
+        Ok(())
     }
 
     async fn add_track_to_queue(&mut self, id: u32) -> Result<()> {
         let mut tracklist = self.tracklist_rx.borrow().clone();
         let track = self.client.track(id).await?;
 
+        let notification = Notification::Info(format!("{} added to queue", track.title.clone()));
+
         tracklist.queue.push(track);
-        self.update_queue(tracklist).await
+        self.update_queue(tracklist).await?;
+        self.broadcast.send(notification);
+        Ok(())
     }
 
     async fn play_track_next(&mut self, id: u32) -> Result<()> {
         let mut tracklist = self.tracklist_rx.borrow().clone();
         let track = self.client.track(id).await?;
 
+        let notification = Notification::Info(format!("{} playing next", track.title.clone()));
+
         let current_index = tracklist.current_position();
         tracklist.queue.insert(current_index + 1, track);
-        self.update_queue(tracklist).await
+        self.update_queue(tracklist).await?;
+        self.broadcast.send(notification);
+        Ok(())
     }
 
     async fn reorder_queue(&mut self, new_order: Vec<usize>) -> Result<()> {
@@ -440,7 +451,10 @@ impl Player {
 
         tracklist.queue = reordered;
 
-        self.update_queue(tracklist).await
+        self.update_queue(tracklist).await?;
+        let notification = Notification::Info("Queue updated".into());
+        self.broadcast.send(notification);
+        Ok(())
     }
 
     async fn tick(&mut self) -> Result<()> {

qobuz-player-controls/src/simple_cache.rs
@@ -2,7 +2,7 @@ use time::Duration;
 use tokio::{sync::RwLock, time::Instant};
 
 #[derive(Debug)]
-pub(crate) struct SimpleCache<T> {
+pub struct SimpleCache<T> {
     value: RwLock<Option<T>>,
     ttl: Duration,
     created: RwLock<Option<Instant>>,

commit 1b8160be0caf64182057817b8a96e30d7538c6e7

Fixed regression from #227. Closes #238

qobuz-player-tui/src/app.rs
@@ -81,8 +81,9 @@ pub enum Output {
     NotConsumed,
     UpdateFavorites,
     Popup(Popup),
-    PopPoputUpdateFavorites,
-    AddTrackToPlaylist(Track),
+    PopPopupUpdateFavorites,
+    AddTrackToPlaylistPopup(Track),
+    AddTrackToPlaylistAndPopPopup((u32, u32)), // TODO: Add a type
 }
 
 #[derive(Default, PartialEq)]
@@ -238,7 +239,6 @@ impl App {
                 self.update_favorites().await;
                 self.should_draw = true;
             }
-
             Output::NotConsumed => match key_code {
                 KeyCode::Char('?') => {
                     self.app_state = AppState::Help;
@@ -301,7 +301,7 @@ impl App {
                 self.app_state = AppState::Popup(popups);
                 self.should_draw = true;
             }
-            Output::PopPoputUpdateFavorites => {
+            Output::PopPopupUpdateFavorites => {
                 if let AppState::Popup(popups) = &mut self.app_state {
                     popups.pop();
                     if popups.is_empty() {
@@ -311,7 +311,7 @@ impl App {
                     self.should_draw = true;
                 }
             }
-            Output::AddTrackToPlaylist(track) => {
+            Output::AddTrackToPlaylistPopup(track) => {
                 let playlists_res = self.client.favorites().await.map(|favs| {
                     favs.playlists
                         .into_iter()
@@ -333,6 +333,30 @@ impl App {
                     self.app_state = AppState::Popup(popups);
                     self.should_draw = true;
                 }
+            }
+            Output::AddTrackToPlaylistAndPopPopup((track_id, playlist_id)) => {
+                match self
+                    .client
+                    .playlist_add_track(playlist_id, &[track_id])
+                    .await
+                {
+                    Ok(_) => {
+                        if let AppState::Popup(popups) = &mut self.app_state {
+                            popups.pop();
+                            if popups.is_empty() {
+                                self.app_state = AppState::Normal;
+                            }
+                            self.update_favorites().await;
+                        }
+                        self.notifications
+                            .push(Notification::Info("Added to playlist".into())); // Add track and playlist name
+                    }
+                    Err(err) => {
+                        self.notifications
+                            .push(Notification::Error(err.to_string()));
+                    }
+                };
+                self.should_draw = true;
             }
         }
     }

qobuz-player-tui/src/popup.rs
@@ -128,6 +128,14 @@ impl TrackPopupState {
             track,
         }
     }
+
+    fn select_next(&mut self) {
+        self.playlists.select_next();
+    }
+
+    fn select_previous(&mut self) {
+        self.playlists.select_previous();
+    }
 }
 
 pub struct NewPlaylistPopupState {
@@ -371,19 +379,39 @@ impl Popup {
                             .await
                     }
                 },
-                Popup::Track(track_popup_state) => {
-                    track_popup_state
-                        .playlists
-                        .handle_events(key_event.code, client, notifications)
-                        .await
-                }
+                Popup::Track(track_popup_state) => match key_event.code {
+                    KeyCode::Up | KeyCode::Char('k') => {
+                        track_popup_state.select_previous();
+                        Ok(Output::Consumed)
+                    }
+                    KeyCode::Down | KeyCode::Char('j') => {
+                        track_popup_state.select_next();
+                        Ok(Output::Consumed)
+                    }
+                    KeyCode::Enter => {
+                        let index = track_popup_state.playlists.selected();
+                        let id = index
+                            .and_then(|index| track_popup_state.playlists.get(index))
+                            .map(|p| p.id);
+
+                        if let Some(id) = id {
+                            return Ok(Output::AddTrackToPlaylistAndPopPopup((
+                                track_popup_state.track.id,
+                                id,
+                            )));
+                        }
+
+                        Ok(Output::Consumed)
+                    }
+                    _ => Ok(Output::NotConsumed),
+                },
                 Popup::NewPlaylist(state) => match key_event.code {
                     KeyCode::Enter => {
                         let input = state.name.value();
                         client
                             .create_playlist(input.to_string(), false, Default::default(), None)
                             .await?;
-                        Ok(Output::PopPoputUpdateFavorites)
+                        Ok(Output::PopPopupUpdateFavorites)
                     }
                     _ => {
                         state.name.handle_event(&event);
@@ -394,10 +422,10 @@ impl Popup {
                     KeyCode::Enter => {
                         if state.confirm {
                             client.delete_playlist(state.id).await?;
-                            return Ok(Output::PopPoputUpdateFavorites);
+                            return Ok(Output::PopPopupUpdateFavorites);
                         }
 
-                        Ok(Output::PopPoputUpdateFavorites)
+                        Ok(Output::PopPopupUpdateFavorites)
                     }
                     KeyCode::Left | KeyCode::Right => {
                         state.confirm = !state.confirm;

qobuz-player-tui/src/widgets/playlist_list.rs
@@ -43,6 +43,22 @@ impl PlaylistList {
         self.items.set_all_items(items);
     }
 
+    pub fn selected(&self) -> Option<usize> {
+        self.items.state.selected()
+    }
+
+    pub fn get(&self, index: usize) -> Option<&Playlist> {
+        self.items.filter().get(index)
+    }
+
+    pub fn select_next(&mut self) {
+        self.items.state.select_next();
+    }
+
+    pub fn select_previous(&mut self) {
+        self.items.state.select_previous();
+    }
+
     pub async fn handle_events(
         &mut self,
         event: KeyCode,

qobuz-player-tui/src/widgets/track_list.rs
@@ -84,7 +84,7 @@ impl TrackList {
                 let track = index.and_then(|index| self.items.filter().get(index));
 
                 if let Some(id) = track {
-                    return Ok(Output::AddTrackToPlaylist(id.clone()));
+                    return Ok(Output::AddTrackToPlaylistPopup(id.clone()));
                 }
                 Ok(Output::Consumed)
             }