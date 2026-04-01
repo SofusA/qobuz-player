448392d95585b01f77a164a00a52f66f5e0ea897
Sofus Addington <sofus@addington.dk>
2026-04-01 12:37:18 +0200

    gtk app

---
+++ qobuz-player-gtk/src/main.rs
@@ -0,0 +1,139 @@
+use qobuz_player_cli::{
+    ConnectArgs, SharedArgs, SharedCommands, create_player, default_audio_quality, get_client,
+    handle_shared_commands, spawn_clean_up,
+};
+use std::sync::Arc;
+use tokio::sync::broadcast;
+
+use clap::Parser;
+use qobuz_player_controls::{
+    AppResult, database::Database, error::Error, notification::NotificationBroadcast,
+};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Arguments {
+    #[clap(flatten)]
+    shared: SharedArgs,
+
+    #[clap(flatten)]
+    connect: ConnectArgs,
+
+    #[clap(subcommand)]
+    command: Option<SharedCommands>,
+}
+
+#[tokio::main]
+async fn main() {
+    match run().await {
+        Ok(()) => {}
+        Err(err) => {
+            error_exit(err);
+        }
+    }
+}
+
+pub async fn run() -> AppResult<()> {
+    let args = Arguments::parse();
+    let database = Arc::new(Database::new().await?);
+
+    if let Some(command) = args.command {
+        handle_shared_commands(command, &database).await?;
+        return Ok(());
+    }
+
+    let (exit_sender, exit_receiver) = broadcast::channel(5);
+
+    let max_audio_quality = default_audio_quality(&database, args.shared.max_audio_quality).await?;
+    let client = get_client(&database, max_audio_quality).await?;
+    let client = Arc::new(client);
+
+    let broadcast = Arc::new(NotificationBroadcast::new());
+
+    let mut player = create_player(
+        args.shared.audio_cache,
+        database.clone(),
+        client.clone(),
+        broadcast.clone(),
+        None,
+        None,
+        args.shared.output_device_id,
+    )
+    .await?;
+
+    #[cfg(target_os = "linux")]
+    {
+        let position_receiver = player.position();
+        let tracklist_receiver = player.tracklist();
+        let volume_receiver = player.volume();
+        let status_receiver = player.status();
+        let controls = player.controls();
+        let exit_sender = exit_sender.clone();
+        tokio::spawn(async move {
+            if let Err(e) = qobuz_player_mpris::init(
+                position_receiver,
+                tracklist_receiver,
+                volume_receiver,
+                status_receiver,
+                controls,
+                exit_sender,
+            )
+            .await
+            {
+                error_exit(e);
+            }
+        });
+    }
+
+    let client = client.clone();
+
+    if args.connect.connect {
+        let app_id = client.app_id().await?;
+        let position_receiver = player.position();
+        let tracklist_receiver = player.tracklist();
+        let volume_receiver = player.volume();
+        let status_receiver = player.status();
+        let controls = player.controls();
+
+        tokio::spawn(async move {
+            if let Err(e) = qobuz_player_connect::init(
+                &app_id,
+                args.connect.name_args.connect_name,
+                controls,
+                position_receiver,
+                tracklist_receiver,
+                status_receiver,
+                volume_receiver,
+                max_audio_quality,
+            )
+            .await
+            {
+                error_exit(e);
+            }
+        });
+    }
+
+    let controls = player.controls();
+    let tracklist_receiver = player.tracklist();
+    let status_receiver = player.status();
+    let position_receiver = player.position();
+    tokio::task::spawn_blocking(move || {
+        qobuz_player_gtk::init(
+            client,
+            tracklist_receiver,
+            status_receiver,
+            position_receiver,
+            controls,
+        );
+    });
+
+    spawn_clean_up(database, args.shared.audio_cache_time_to_live);
+    player.player_loop(exit_receiver).await?;
+
+    Ok(())
+}
+
+fn error_exit(error: Error) {
+    eprintln!("{error}");
+    std::process::exit(1);
+}
